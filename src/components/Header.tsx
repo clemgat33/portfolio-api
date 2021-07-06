d1dbecbe7cf53727ffd6af0231ab77345308294d
cidgi359 <74004967+cidgi359@users.noreply.github.com>
2021-07-06 14:19:55 +0300

    react-helmet-async

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Helmet} from 'react-helmet';
+import {Helmet} from 'react-helmet-async';
 import { Link, withRouter } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
